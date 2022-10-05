b3d936b4e0a55dd1660922c04e57484b068f339f
Ashwin S Shenoy <aswinshenoy65@gmail.com>
2022-10-06 03:28:02 +0530

    Add 404 page, and page redirects

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  swcMinify: true
+  swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/team',
+        destination: '/members',
+        permanent: true
+      },
+      {
+        source: '/about-us',
+        destination: '/about',
+        permanent: true
+      }
+    ];
+  }
 }
 
 module.exports = nextConfig
